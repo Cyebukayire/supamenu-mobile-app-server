850d15610a26cb9bc4850c590d3125184c116aec
Cyebukayire <peacecyebukayire@gmail.com>
2022-07-01 19:57:41 +0200

    create swagger apis

--- src/server.js
+++ src/server.js
@@ -3,17 +3,18 @@ import { DbConnection } from "./database";
 import authMiddleware from "./middlewares/auth.middleware";
 import authRouter from './routes/Auth.route';
 import userRouter from './routes/User.route';
-import swaggerDoc from './swagger';
 import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from './swagger'
+import YAML from "yamljs";
 
 DbConnection();
 
 const app = express();
 const port = process.env.PORT || 4000;
+// const swaggerDoc = YAML.load('src/swagger.yaml');
 
 app.use(express.json());
-app.use('/api/v1/api-docs', swaggerUi.serve);
-app.get('/api/v1/api-docs', swaggerUi.setup(swaggerDoc));
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.get('/api/v1', (req,res) => res.send("Welcome to Server!"));
 app.use('/api/v1/auth', authRouter );
--- src/swagger.js
+++ src/swagger.js
@@ -3,45 +3,94 @@ export default {
     info: {
         description: "User Management System",
         version: "1.0.0",
-        title: "Tasks API",
+        title: "Users API",
         contact: {
             email: "peace@google.com"
+        },
+        license: {
+            "name": "MIT",
+            "url": "https://opensource.org/licenses/MIT"
         }
     },
-    schemes: ["http"],
+    schemes: ["http", "https"],
     host: "localhost:4000",
     basePath: "/api/v1",
+
+    tags: [
+        {
+            name: 'User',
+            description: 'APIs for users'
+        },
+        {
+            name: 'Task',
+            description: 'APIs for tasks'
+        }
+    ],
+
+    definitions: {
+        User: {
+            required: ["name", "email", "password"],
+            properties: {
+                name: {
+                    type: 'string',
+                },
+                email: {
+                    type: 'string'
+                },
+                password: {
+                    type: 'string'
+                },
+                // registered_at:{
+                //     type: 'string'
+                // }
+            }
+        },
+        Error: {
+            title: "Error",
+            type: 'Object',
+            message: {
+                type: 'string'
+            }
+
+        }
+    },
     paths: {
         "/auth/signup": {
             post: {
+                tags: ['User'],
                 summary: "Create an a new user",
-                description: "Create new User",
-                produces: ["application/json"],
-                parameters: [],
+                parameters: [
+                    {
+                        name: 'user',
+                        in: 'body',
+                        description: 'User to register',
+                        schema: {
+                            $ref: '#/definitions/User'
+                        }
+                    }
+                ],
+                produces: ['application/json'],
                 response: {
                     200: {
                         description: "Successul Operation",
                         schema: {
-                            type: "array",
-                            // items: {
-                            //     $ref: "#/definitions/signupResponse"
-                            // }
+                            $ref: "#/definitions/User"
                         }
                     },
                     401: {
                         description: "Unauthorized",
                         schema: {
-                            // $ref: "#/definition/unauthorizedResponse"
+                            $ref: "#/definitions/Error"
                         }
                     },
                     400: {
                         description: "Bad Request",
                         schema: {
-                            // $ref: "#/definition/badrequestResponse"
+                            $ref: "#/definitions/Error"
                         }
                     }
                 }
             }
-        }
+        },
     }
 }
